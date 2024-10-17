5eab43995ce87d234213865290b44252a56e6b18
Hibi4 <144681439+Hibi4@users.noreply.github.com>
2024-10-17 17:49:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ portfolio/src/pages/i18n/ui.ts
@@ -0,0 +1,45 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.projects': 'Projects',
+      'nav.about': 'About',
+      'nav.education': 'Education',
+      'nav.contact': 'Contact',
+      'english': 'English',
+      'french': 'French',
+    },
+    fr: {
+      'nav.home': 'Accueil',
+      'nav.projects': 'Projets',
+      'nav.about': 'À propos',
+      'nav.education': 'Éducation',
+      'nav.contact': 'Contact',
+      'english': 'Anglais',
+      'french': 'Français',
+    },
+} as const;
+/* export const languages = {
+    en: 'English',
+    fr: 'Français',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.twitter': 'Twitter',
+    },
+    fr: {
+      'nav.home': 'Accueil',
+      'nav.about': 'À propos',
+    },
+  } as const; */ 
